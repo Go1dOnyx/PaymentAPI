60fc9fad939c59534523fec0eafd8afa93454b94
Jesus Diaz <jdiazndca21@gmail.com>
2024-03-04 22:30:36 -0600

    Commit Changes

--- ResourceAPI/Controllers/AccountController.cs
+++ ResourceAPI/Controllers/AccountController.cs
@@ -2,39 +2,54 @@
 using ResourceAPI.EF.DbContexts;
 using ResourceAPI.EF.Models;
 using ResourceAPI.EF.Repositories;
+using ResourceAPI.Models;
 using System.Runtime.CompilerServices;
 
 namespace ResourceAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountController : Controller
+    public class AccountController : ControllerBase
     {
         private readonly PaymentsContext _paymentsContext;
         private UserRepository _userRepository;
-        public AccountController(PaymentsContext dbContext) 
+        public AccountController(PaymentsContext dbContext, UserRepository userRepo) 
         {
             _paymentsContext = dbContext;
             _userRepository = new UserRepository(_paymentsContext);
         }
 
-        [HttpPost(Name = "CreateAccount")]
-        public IActionResult PostUser()
+        [HttpPost("login")]
+        public IActionResult Login(LoginModel model)
         {
             return View();
         }
-        public IActionResult PutUser() 
+
+        [HttpPost("register")]
+        public IActionResult Register() 
         {
             return View();
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Update() 
+        {
+            return View();
+        }
+
+        [HttpDelete("{id}")]
         public IActionResult DeleteUser() 
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult GetUser() 
         {
             return View();
         }
+
+        [HttpGet]
         public IActionResult GetAllUsers() 
         {
             return View();
